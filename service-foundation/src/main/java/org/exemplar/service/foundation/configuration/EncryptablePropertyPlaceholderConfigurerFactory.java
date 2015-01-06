c20cdc2349747e5948623df0909a52b3784470a4
nebojsa <Bebezinho09>
2015-01-06 22:32:53 +0000

    using EncryptablePropertyPlaceholderConfigurer for property loading

---
+++ service-foundation/src/main/java/org/exemplar/service/foundation/configuration/EncryptablePropertyPlaceholderConfigurerFactory.java
@@ -0,0 +1,70 @@
+package org.exemplar.service.foundation.configuration;
+
+import com.google.common.base.Function;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.spring31.properties.EncryptablePropertyPlaceholderConfigurer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.util.List;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+public class EncryptablePropertyPlaceholderConfigurerFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EncryptablePropertyPlaceholderConfigurerFactory.class);
+    private static final String DEFAULT_ENVIRONMENT = "local";
+    private static final String DEFAULT_ENCRYPT_PASSWORD = "t0Ps3cr3t";
+
+    public static EncryptablePropertyPlaceholderConfigurer create(String serviceName) {
+        EncryptablePropertyPlaceholderConfigurer configurer = new EncryptablePropertyPlaceholderConfigurer(encryptor());
+        configurer.setLocations(getResourceLocations(serviceName, getEnvironment()));
+        configurer.setIgnoreResourceNotFound(true);
+        return configurer;
+    }
+
+    public static StandardPBEStringEncryptor encryptor() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        String encryptPassword = System.getProperty("enc.password");
+        encryptor.setPassword(
+                isNullOrEmpty(encryptPassword) ? DEFAULT_ENCRYPT_PASSWORD : encryptPassword
+        );
+        return encryptor;
+    }
+
+    private static Resource[] getResourceLocations(String serviceName, String environment) {
+        List<String> locations = getFileLocations(serviceName, environment);
+
+        List<Resource> resources = Lists.transform(locations, new Function<String, Resource>() {
+            @Override
+            public Resource apply(String location) {
+                return new ClassPathResource(location);
+            }
+        });
+
+        return resources.toArray(new Resource[resources.size()]);
+    }
+
+    private static List<String> getFileLocations(String serviceName, String environment) {
+        List<String> fileLocation = Lists.newArrayList();
+        fileLocation.add("config/default.properties");
+        fileLocation.add(String.format("config/global.%s.properties", environment));
+        fileLocation.add(String.format("config/%s.default.properties", serviceName));
+        fileLocation.add(String.format("config/%s.%s.properties", serviceName, environment));
+        LOGGER.info("Loading properties from {}", fileLocation);
+        return fileLocation;
+    }
+
+    private static String getEnvironment() {
+        String environment = System.getProperty("env");
+        if(Strings.isNullOrEmpty(environment)) {
+            environment = DEFAULT_ENVIRONMENT;
+            System.setProperty("env", environment);
+        }
+        return environment;
+    }
+}
--- service-foundation/src/test/java/org/exemplar/service/foundation/configuration/TestConfiguration.java
+++ service-foundation/src/test/java/org/exemplar/service/foundation/configuration/TestConfiguration.java
@@ -1,9 +1,16 @@
 package org.exemplar.service.foundation.configuration;
 
+import org.jasypt.spring31.properties.EncryptablePropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("org.exemplar.service")
 public class TestConfiguration {
+
+    @Bean
+    public EncryptablePropertyPlaceholderConfigurer encryptablePropertyPlaceholderConfigurer() {
+        return EncryptablePropertyPlaceholderConfigurerFactory.create("test");
+    }
 }
